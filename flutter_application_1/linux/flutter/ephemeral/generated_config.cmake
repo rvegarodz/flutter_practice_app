deaaf27b80ce7c2d219053df1297112c2cadbf68
rvegarodz <r.vega27@outlook.com>
2023-04-19 15:05:20 -0400

    adding flutter_practice_app files

---
+++ flutter_application_1/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/rafael/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/rafael/project_files/test_flutter2/flutter_practice_app/flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/rafael/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/rafael/project_files/test_flutter2/flutter_practice_app/flutter_application_1"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/rafael/project_files/test_flutter2/flutter_practice_app/flutter_application_1/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/rafael/project_files/test_flutter2/flutter_practice_app/flutter_application_1/lib/main.dart"
+)
